0623ac45f270ce213017cd50132c454b758d2c7d
chanhou <iepiechan@gmail.com>
2016-06-18 05:23:31 +0800

    havent debug

--- superTeam.py
+++ superTeam.py
@@ -78,7 +78,7 @@ class QLearningAgent(CaptureAgent):
         - self.getLegalActions(state)
           which returns legal actions for a state
     """
-    def __init__(self, index, timeForComputing = .1, numTraining=100, epsilon=0.5, alpha=0.5, gamma=1):
+    def __init__(self, index, timeForComputing = .1, actionFn = None, numTraining=100, epsilon=0.5, alpha=0.5, gamma=1):
         """
         actionFn: Function which takes a state and returns the list of legal actions
 
@@ -88,6 +88,9 @@ class QLearningAgent(CaptureAgent):
         numTraining - number of training episodes, i.e. no learning after these many episodes
         """
         CaptureAgent.__init__(self, index, timeForComputing)
+        if actionFn == None:
+          actionFn = lambda state: state.getLegalActions()
+        self.actionFn = actionFn
         self.episodesSoFar = 0
         self.accumTrainRewards = 0.0
         self.accumTestRewards = 0.0
@@ -100,6 +103,21 @@ class QLearningAgent(CaptureAgent):
     def registerInitialState(self, gameState):
         self.start = gameState.getAgentPosition(self.index)
         CaptureAgent.registerInitialState(self, gameState)
+        self.startEpisode()
+        if self.episodesSoFar == 0:
+          print 'Beginning %d episodes of Training' % (self.numTraining)
+
+    def observationFunction(self, state):
+        """
+            This is where we ended up after our last action.
+            The simulation should somehow ensure this is called
+        """
+        if not self.lastState is None:
+            reward = state.getScore() - self.lastState.getScore()
+            self.observeTransition(self.lastState, self.lastAction, state, reward)
+
+        return CaptureAgent.observationFunction(self, state)
+        # return state
 
     def getQValue(self, state, action):
         """
@@ -117,11 +135,13 @@ class QLearningAgent(CaptureAgent):
           terminal state, you should return a value of 0.0.
         """
         actions = state.getLegalActions(self.index)
-    
+        
+        if len(actions) == 0:
+          return 0.0
         # You can profile your evaluation time by uncommenting these lines
         # start = time.time()
-        values = [self.getQValue(gameState, a) for a in actions]
-        # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
+        values = [self.getQValue(state, a) for a in actions]
+        # print 'eval time for agent %:wd: %.4f' % (self.index, time.time() - start)
     
         return max(values)
 
@@ -140,7 +160,8 @@ class QLearningAgent(CaptureAgent):
 
         maxValue = max(values)
         bestActions = [a for a, v in zip(actions, values) if v == maxValue]
-
+        if len(bestActions) == 0:
+          return None
         return random.choice(bestActions)
 
     def chooseAction(self, gameState):
@@ -156,10 +177,13 @@ class QLearningAgent(CaptureAgent):
         """
         # Pick Action
         actions = gameState.getLegalActions(self.index)
+        action = None
         if util.flipCoin(self.epsilon):
           action = random.choice(actions)
         else:
           action = self.computeActionFromQValues(gameState)
+
+        self.doAction(gameState,action) # from Q learning agent
         return action
 
     def update(self, state, action, nextState, reward):
@@ -179,6 +203,94 @@ class QLearningAgent(CaptureAgent):
     def getValue(self, state):
         return self.computeValueFromQValues(state)
 
+    def getLegalActions(self,state):
+        """
+          Get the actions available for a given
+          state. This is what you should use to
+          obtain legal actions for a state
+        """
+        return self.actionFn(state)
+
+    def observeTransition(self, state,action,nextState,deltaReward):
+        """
+            Called by environment to inform agent that a transition has
+            been observed. This will result in a call to self.update
+            on the same arguments
+
+            NOTE: Do *not* override or call this function
+        """
+        self.episodeRewards += deltaReward
+        self.update(state,action,nextState,deltaReward)
+
+    def startEpisode(self):
+        """
+          Called by environment when new episode is starting
+        """
+        self.lastState = None
+        self.lastAction = None
+        self.episodeRewards = 0.0
+
+    def stopEpisode(self):
+        """
+          Called by environment when episode is done
+        """
+        if self.episodesSoFar < self.numTraining:
+            self.accumTrainRewards += self.episodeRewards
+        else:
+            self.accumTestRewards += self.episodeRewards
+        self.episodesSoFar += 1
+        if self.episodesSoFar >= self.numTraining:
+            # Take off the training wheels
+            self.epsilon = 0.0    # no exploration
+            self.alpha = 0.0      # no learning
+
+    def isInTraining(self):
+        return self.episodesSoFar < self.numTraining
+
+    def isInTesting(self):
+        return not self.isInTraining()
+
+    def final(self, state):
+        """
+          Called by Pacman game at the terminal state
+        """
+        CaptureAgent.final(self, state)
+        deltaReward = state.getScore() - self.lastState.getScore()
+        self.observeTransition(self.lastState, self.lastAction, state, deltaReward)
+        self.stopEpisode()
+
+        # Make sure we have this var
+        if not 'episodeStartTime' in self.__dict__:
+            self.episodeStartTime = time.time()
+        if not 'lastWindowAccumRewards' in self.__dict__:
+            self.lastWindowAccumRewards = 0.0
+        self.lastWindowAccumRewards += state.getScore()
+
+        NUM_EPS_UPDATE = 100
+        if self.episodesSoFar % NUM_EPS_UPDATE == 0:
+            print 'Reinforcement Learning Status:'
+            windowAvg = self.lastWindowAccumRewards / float(NUM_EPS_UPDATE)
+            if self.episodesSoFar <= self.numTraining:
+                trainAvg = self.accumTrainRewards / float(self.episodesSoFar)
+                print '\tCompleted %d out of %d training episodes' % (
+                       self.episodesSoFar,self.numTraining)
+                print '\tAverage Rewards over all training: %.2f' % (
+                        trainAvg)
+            else:
+                testAvg = float(self.accumTestRewards) / (self.episodesSoFar - self.numTraining)
+                print '\tCompleted %d test episodes' % (self.episodesSoFar - self.numTraining)
+                print '\tAverage Rewards over testing: %.2f' % testAvg
+            print '\tAverage Rewards for last %d episodes: %.2f'  % (
+                    NUM_EPS_UPDATE,windowAvg)
+            print '\tEpisode took %.2f seconds' % (time.time() - self.episodeStartTime)
+            self.lastWindowAccumRewards = 0.0
+            self.episodeStartTime = time.time()
+
+        if self.episodesSoFar == self.numTraining:
+            msg = 'Training Done (turning off epsilon and alpha)'
+            print '%s\n%s' % (msg,'-' * len(msg))
+
+
 class ApproximateQAgent(QLearningAgent):
     """
        ApproximateQLearningAgent
@@ -187,7 +299,7 @@ class ApproximateQAgent(QLearningAgent):
        and update.  All other QLearningAgent functions
        should work as is.
     """
-    def __init__(self, index, timeForComputing = .1, extractor='IdentityExtractor', **args):
+    def __init__(self, index, timeForComputing = .1, extractor='SimpleExtractor', **args):
         QLearningAgent.__init__(self, index, timeForComputing, **args)
         self.featExtractor = util.lookup(extractor, globals())()
         self.weights = util.Counter()
@@ -212,7 +324,8 @@ class ApproximateQAgent(QLearningAgent):
 
         # You can profile your evaluation time by uncommenting these lines
         # start = time.time()
-        values = [self.evaluate(gameState, a) for a in actions]
+        # values = [self.evaluate(nextState, a) for a in actions]
+        values = [self.getQValue(nextState, a) for a in actions]
         # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
 
         maxValue = max(values)
@@ -226,7 +339,7 @@ class ApproximateQAgent(QLearningAgent):
     def final(self, state):
         "Called at the end of each game."
         # call the super-class final method
-        # QLearningAgent.final(self, state)
+        QLearningAgent.final(self, state)
 
         # did we finish training?
         if self.episodesSoFar == self.numTraining:
